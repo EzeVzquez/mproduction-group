4b3f55901b5e8ac719cdadaa7cda9f487699aa81
EzeVzquez <ezequielvazquez33@outlook.com>
2022-08-17 18:32:10 -0300

    add go to top button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 const $menuButton = document.querySelector(".menu-button-container");
+const $goToTopButton = document.getElementById("myBtn");
 
 $menuButton.addEventListener("click", () => $menuButton.classList.toggle("clicked") );
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    $goToTopButton.style.display = "block";
+  } else {
+    $goToTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener ("scroll" ,scrollFunction)
+
+$goToTopButton.addEventListener("click", topFunction)
